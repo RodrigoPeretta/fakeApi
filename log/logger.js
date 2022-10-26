6269e489c3680852b54439ab53b296a9e83b7497
Rodrigo Peretta da Silva <rperetta@img.com.br>
2022-10-25 23:30:20 -0300

    Mudanca no arquivo de log

--- log/logger.js
+++ log/logger.js
@@ -19,8 +19,12 @@ morgan.token('date', (req, res, tz) => {
   return moment().format('DD-MM-YYYY, HH:mm');
 });
 
+morgan.token("response-body", (req, res) => {
+  const body = { ...JSON.parse(res.responseBody) };
+  return JSON.stringify(body);
+});
 
-loggerConfig.use(morgan(`[:date] :method :url | :status | :response-time ms | Params: :params Body: :body`, 
+loggerConfig.use(morgan(`[:date] :method :url | :status | :response-time ms | Params: :params Body: :body | Response: :response-body`, 
 { 
   stream: accessLogStream, 
   skip: (req, res) => req.url === '/readLog' || req.url === '/clearLog'
